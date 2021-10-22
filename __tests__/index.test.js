ead0c963cb475d13a6009991670ae3395ff6e2a7
Scott Vandehey <fake-scott@spaceninja.com>
2021-10-22 09:40:53 -0700

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,8 +4,10 @@ const config = require('../');
 const fs = require('fs');
 const stylelint = require('stylelint');
 
-const validCss = fs.readFileSync('./__tests__/css-valid.css', 'utf-8');
-const invalidCss = fs.readFileSync('./__tests__/css-invalid.css', 'utf-8');
+const validCss = fs.readFileSync('./__tests__/valid.css', 'utf-8');
+const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf-8');
+const validScss = fs.readFileSync('./__tests__/valid.scss', 'utf-8');
+const invalidScss = fs.readFileSync('./__tests__/invalid.scss', 'utf-8');
 
 describe('flags no warnings with valid css', () => {
 	let result;
@@ -13,6 +15,25 @@ describe('flags no warnings with valid css', () => {
 	beforeEach(() => {
 		result = stylelint.lint({
 			code: validCss,
+			config,
+		});
+	});
+
+	it('did not error', () => {
+		return result.then((data) => expect(data.errored).toBeFalsy());
+	});
+
+	it('flags no warnings', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(0));
+	});
+});
+
+describe('flags no warnings with valid scss', () => {
+	let result;
+
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: validScss,
 			config,
 		});
 	});
@@ -68,5 +89,50 @@ describe('flags warnings with invalid css', () => {
 
 	it('correct column number', () => {
 		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(8));
+	});
+});
+
+describe('flags warnings with invalid scss', () => {
+	let result;
+
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: invalidScss,
+			config,
+		});
+	});
+
+	it('did error', () => {
+		return result.then((data) => expect(data.errored).toBeTruthy());
+	});
+
+	it('flags one warning', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(1));
+	});
+
+	it('correct warning text', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].text).toBe(
+				'Expected @if not statement rather than @if statement == null (scss/at-if-no-null)',
+			),
+		);
+	});
+
+	it('correct rule flagged', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].rule).toBe('scss/at-if-no-null'),
+		);
+	});
+
+	it('correct severity flagged', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
+	});
+
+	it('correct line number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].line).toBe(1));
+	});
+
+	it('correct column number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(1));
 	});
 });
